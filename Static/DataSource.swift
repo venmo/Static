08b91a94e1745236253dc3c3471b6649bcf2ef84
Derek Liu <liuduo1988@gmail.com>
2015-09-08 18:15:30 +0800

    add judgement of table is nil

--- Static/DataSource.swift
+++ Static/DataSource.swift
@@ -120,6 +120,8 @@ public class DataSource: NSObject {
     }
 
     private func refreshRegisteredCells() {
+        guard let tableView = tableView else { return }
+        
         // Filter to only rows with unregistered cells
         let rows = sections.map({ $0.rows }).reduce([], combine: +).filter() {
             !self.registeredCellIdentifiers.contains($0.cellIdentifier)
@@ -134,7 +136,7 @@ public class DataSource: NSObject {
             }
 
             registeredCellIdentifiers.insert(identifier)
-            tableView?.registerClass(row.cellClass, forCellReuseIdentifier: identifier)
+            tableView.registerClass(row.cellClass, forCellReuseIdentifier: identifier)
         }
     }
 }
