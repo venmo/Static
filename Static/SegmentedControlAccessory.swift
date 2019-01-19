b1e18359666443f8a02435a034a50adf0a4b270f
Phillip Kast <phil@yearofcode.com>
2019-01-18 18:32:53 -0800

    Make SegmentedControlAccessory open

--- Static/SegmentedControlAccessory.swift
+++ Static/SegmentedControlAccessory.swift
@@ -1,10 +1,10 @@
 import UIKit
 
-class SegmentedControlAccessory: UISegmentedControl {
-    typealias ValueChange = (Int, Any?) -> ()
+open class SegmentedControlAccessory: UISegmentedControl {
+    public typealias ValueChange = (Int, Any?) -> ()
     var valueChange: ValueChange? = nil
     
-    init(items: [Any], selectedIndex: Int, valueChange: (ValueChange)? = nil) {
+    public init(items: [Any], selectedIndex: Int, valueChange: (ValueChange)? = nil) {
         super.init(items: items)
         
         self.valueChange = valueChange
@@ -12,10 +12,10 @@ class SegmentedControlAccessory: UISegmentedControl {
         addTarget(self, action: #selector(valueChanged), for: .valueChanged)
     }
     
-    fileprivate init() {super.init(frame: .zero)}
-    fileprivate override init(frame: CGRect) {super.init(frame: frame)}
+    public init() {super.init(frame: .zero)}
+    public override init(frame: CGRect) {super.init(frame: frame)}
     
-    required init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
