2b8c38c904acfe30b8def63a96cac01bd433f4ae
Sam Soffes <sam@soff.es>
2020-06-03 13:41:08 -0700

    Add the platform

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Static",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
         .library(name: "Static", targets: ["Static"])
     ],
