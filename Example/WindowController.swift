556016b7311f23f6e94408e54c61f9e98bc5dd02
Carl Hill-Popper <chillpopper@paypal.com>
2021-10-21 15:04:56 -0500

    Make window a stored property
    Fixes a console warning
    "UIWindows were created prior to initial application activation. This may result in incorrect visual appearance."

--- Example/WindowController.swift
+++ Example/WindowController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 @UIApplicationMain final class WindowController: UIResponder {
-    var window: UIWindow? = {
+    lazy var window: UIWindow? = {
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = UINavigationController(rootViewController: ViewController())
         return window
